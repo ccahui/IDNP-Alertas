a6337448df18f1e06f725ccab3555693f3a27624
ccahui <kris_ti4n@hotmail.com>
2019-11-06 17:23:30 -0500

    Mapa

--- app/src/main/java/com/example/proyecto/ui/ubicacion/UbicacionFragment.java
+++ app/src/main/java/com/example/proyecto/ui/ubicacion/UbicacionFragment.java
@@ -13,23 +13,45 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.proyecto.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UbicacionFragment extends Fragment {
+public class UbicacionFragment extends Fragment implements OnMapReadyCallback {
 
     private UbicacionViewModel notificationsViewModel;
+    private GoogleMap mMap;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(UbicacionViewModel.class);
         View root = inflater.inflate(R.layout.fragment_ubicacion, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+
+
+
+        /*final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+
+         */
         return root;
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
