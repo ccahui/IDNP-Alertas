cfa2fd7fb57e67016935c3322ef79319a37e7858
JesusOCH <44234311+JesusOCH@users.noreply.github.com>
2019-11-19 23:04:24 -0500

    Finalizando Funcionalidad de Alertas y de subir archivos

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,55 +1,53 @@
 package com.example.proyecto;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.example.proyecto.Interfaces.IComunicaFragments;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.proyecto.Interfaces.IComunicaFragments;
+import com.google.android.gms.dynamic.IFragmentWrapper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity implements IComunicaFragments {
-
-    @Override
-    public void robo() {
-        Toast.makeText(getApplicationContext(),"Alerta Robo desde el activity", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void vandalismo() {
-        Toast.makeText(getApplicationContext(),"Alerta Vandalismo desde el activity", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void pelea() {
-        Toast.makeText(getApplicationContext(),"Alerta Pelea desde el activity", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void acoso() {
-        Toast.makeText(getApplicationContext(),"Alerta Acoso desde el activity", Toast.LENGTH_SHORT).show();
-    }
+    private static final int RC_GET_IMG = 0;
+    private static final int RC_ACCESS = 1;
+    ImageView img;
+    private Uri fileURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
+        img = (ImageView) findViewById(R.id.pd_imagen);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
@@ -57,28 +55,167 @@ public class MainActivity extends AppCompatActivity implements IComunicaFragment
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
-        case R.id.login:
-            Intent intent = new Intent(this, IniciarSesion.class);
-            startActivity(intent);
-            return(true);
-        case R.id.logout:
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.login:
+                Intent intent = new Intent(this, IniciarSesion.class);
+                startActivity(intent);
+                return (true);
 
-            Intent intent2 = new Intent(this, MapsActivity.class);
-            startActivity(intent2);
-            //finish();
-            return(true);
-    }
-        return(super.onOptionsItemSelected(item));
+            case R.id.logout:
+                Intent intent2 = new Intent(this, MapsActivity.class);
+                startActivity(intent2);
+                return (true);
+        }
+        return (super.onOptionsItemSelected(item));
     }
 
+    public void getImage(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(Intent.createChooser(intent,"Seleccione Imagen"),RC_GET_IMG);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_GET_IMG && resultCode == RESULT_OK){
+            fileURI = data.getData();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},RC_ACCESS);
+            else{
+                InputStream is = null;
+                try{
+                    is = getContentResolver().openInputStream(fileURI);
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    img.setImageBitmap(bitmap);
+                    is.close();
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RC_ACCESS){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                InputStream is = null;
+                try {
+                    is = getContentResolver().openInputStream(fileURI);
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    img.setImageBitmap(bitmap);
+                    is.close();
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void robo() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View v = inflater.inflate(R.layout.alerta_dialog, null);
+        builder.setView(v);
+        AlertDialog alert = builder.create();
+        alert.show();
+        Button signin = (Button) v.findViewById(R.id.entrar_boton);
+        signin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "ALERTA ENVIADA", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }
+
+        );
+
+    }
+
+    @Override
+    public void vandalismo() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View v = inflater.inflate(R.layout.alerta_dialog, null);
+        builder.setView(v);
+        AlertDialog alert = builder.create();
+        alert.show();
+        Button signin = (Button) v.findViewById(R.id.entrar_boton);
+        signin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "ALERTA ENVIADA", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }
+
+        );
+    }
+
+    @Override
+    public void pelea() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View v = inflater.inflate(R.layout.alerta_dialog, null);
+        builder.setView(v);
+        AlertDialog alert = builder.create();
+        alert.show();
+        Button signin = (Button) v.findViewById(R.id.entrar_boton);
+        signin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "ALERTA ENVIADA", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }
+
+        );
+    }
+
+    @Override
+    public void acoso() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View v = inflater.inflate(R.layout.alerta_dialog, null);
+        builder.setView(v);
+        AlertDialog alert = builder.create();
+        alert.show();
+        Button signin = (Button) v.findViewById(R.id.entrar_boton);
+        signin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "ALERTA ENVIADA", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }
+
+        );
+    }
 
 
 }
