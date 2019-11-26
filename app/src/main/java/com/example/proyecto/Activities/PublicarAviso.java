6f90d5e4ded38ee392f664532ad832ef38cb3457
JesusOCH <44234311+JesusOCH@users.noreply.github.com>
2019-11-26 12:30:19 -0500

    Subir datos a Firebase de Publicar Aviso

--- app/src/main/java/com/example/proyecto/Activities/PublicarAviso.java
+++ app/src/main/java/com/example/proyecto/Activities/PublicarAviso.java
@@ -8,6 +8,8 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,24 +18,82 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.example.proyecto.Model.Aviso;
 import com.example.proyecto.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 public class PublicarAviso extends AppCompatActivity {
     private static final int RC_GET_IMG = 0;
     private static final int RC_ACCESS = 1;
     ImageView img;
     private Uri fileURI;
+    private EditText nombre;
+    private EditText apellido;
+    private EditText descripcion;
+    private Button btnaviso;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publicar_aviso);
+        nombre = (EditText) findViewById(R.id.editTextNombres);
+        apellido = (EditText) findViewById(R.id.editTextApellidos);
+        descripcion = (EditText) findViewById(R.id.editTextDescripcion);
         img = (ImageView) findViewById(R.id.pd_imagen);
+        inicializarFirebase();
+        btnaviso = (Button) findViewById(R.id.buttonaviso);
+        btnaviso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nombre.getText().toString().equals("") || apellido.getText().toString().equals("") || descripcion.getText().toString().equals(""))
+                    validacion();
+                else {
+                    Aviso aviso = new Aviso();
+                    aviso.setUid(UUID.randomUUID().toString());
+                    aviso.setNombre(nombre.getText().toString());
+                    aviso.setApellido(apellido.getText().toString());
+                    aviso.setDescripcion(descripcion.getText().toString());
+                    aviso.setImagen(fileURI.getLastPathSegment());
+                    databaseReference.child("Aviso").child(aviso.getUid()).setValue(aviso);
+                    limpiarcajas();
+                }
+            }
+        });
     }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    private void validacion() {
+        String nom = nombre.getText().toString();
+        String ape = apellido.getText().toString();
+        String des = descripcion.getText().toString();
+        if (nom.equals(""))
+            nombre.setError("Required");
+        if (ape.equals(""))
+            apellido.setError("Required");
+        if (des.equals(""))
+            descripcion.setError("Required");
+    }
+
+    private void limpiarcajas() {
+        nombre.setText("");
+        apellido.setText("");
+        descripcion.setText("");
+    }
+
 
     public void getImage(View view) {
         Intent intent = new Intent();
---
+++ app/src/main/java/com/example/proyecto/Model/Aviso.java
@@ -0,0 +1,51 @@
+package com.example.proyecto.Model;
+
+import android.net.Uri;
+
+public class Aviso {
+    private String uid;
+    private String nombre;
+    private String apellido;
+    private String descripcion;
+    private String imagen;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
