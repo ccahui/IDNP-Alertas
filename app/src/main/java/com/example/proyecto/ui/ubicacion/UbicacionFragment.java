9e437e8e7791f11033539e3d3a7d2d5f6f23fd7c
ccahui <kccahui@unsa.edu.pe>
2019-11-27 00:55:50 -0500

    Google Maps Cambiando la ubicacion de punto de Inicio

--- app/src/main/java/com/example/proyecto/ui/ubicacion/UbicacionFragment.java
+++ app/src/main/java/com/example/proyecto/ui/ubicacion/UbicacionFragment.java
@@ -1,10 +1,14 @@
 package com.example.proyecto.ui.ubicacion;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,14 +17,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.proyecto.R;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UbicacionFragment extends Fragment implements OnMapReadyCallback {
+public class UbicacionFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
 
@@ -41,9 +48,51 @@ public class UbicacionFragment extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-9, -75);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Perú"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // TODO: Before enabling the My Location layer, you must request
+        // location permission from the user. This sample does not include
+        // a request for location permission.
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
 
+        int zoom = 17;
+        LatLng ubicacion = new LatLng(-16.406839 , -71.52239);
+        MarkerOptions positionMarker = new MarkerOptions().position(ubicacion).title("Mi Ubicacion").snippet("Descripcion Breve");
+        mMap.addMarker(positionMarker);
+
+
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, zoom));
+
+    }
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this.getContext(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this.getContext(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        int zoom = 17;
+        LatLng ubicacion = new LatLng(-16.406839 , -71.52239);
+        MarkerOptions positionMarker = new MarkerOptions().position(ubicacion).title("Mi Ubicacion").snippet("Descripcion Breve");
+        mMap.addMarker(positionMarker);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, zoom));
+
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(ubicacion)      // Sets the center of the map to location user
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        return true;
     }
 }
